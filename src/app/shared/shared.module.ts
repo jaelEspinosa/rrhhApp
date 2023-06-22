b9da195a777fa9556609475c0ae5ebea35f9260a
jaelespinosa <jaelespinosa@gmail.com>
2023-06-22 15:01:46 +0200

    'ok'

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './foooter/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
